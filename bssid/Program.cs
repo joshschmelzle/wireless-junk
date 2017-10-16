d12d61e3aa22252af32311c3610fd3121ec39f58
Josh Schmelzle <joshuaschmelzle@gmail.com>
2017-10-15 23:03:14 -0400

    added support for toast messages on connect, disconnect and roam events. needs both reviewal and refactor. bug when switching SSIDs causes crash.

--- bssid/Program.cs
+++ bssid/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using Windows.UI.Notifications;
 
 namespace bssid
@@ -11,9 +13,12 @@ namespace bssid
             Console.ReadKey();
         }
         
+        private static SSID ssid;
+        private static bool connected = false;
+        
         private static void showInterfaceInformation()
         {
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            Process p = new Process();
             p.StartInfo.FileName = "netsh.exe";
             p.StartInfo.Arguments = "wlan show interfaces";
             p.StartInfo.UseShellExecute = false;
@@ -24,16 +29,94 @@ namespace bssid
 
             if (!output.Contains("disconnected"))
             {
-                string message = "Wi-Fi connected to " + gv(output, "SSID") + " " + gv(output, "BSSID") + " " + gv(output, "Signal");
+                ssid = new SSID(gv(output, "SSID"), gv(output, "BSSID"));
+                string message = "Wi-Fi connected to " + ssid.Name + " " + ssid.BSSID + " " + gv(output, "Signal");
                 Console.WriteLine(message);
-                ShowToast(message);
-
+                connected = true;
+                ShowToast("Wi-Fi is connected");
                 p.WaitForExit();
             }
             else
             {
                 Console.WriteLine("Wi-Fi is disconnected");
+                connected = false;
+                p.WaitForExit();
             }
+
+            bool createdNew;
+            var waitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "11e370ec-2092-45a2-befd-0946ed24769f", out createdNew);
+            var signaled = false;
+            
+            if (!createdNew)
+            {
+                waitHandle.Set();
+                return;
+            }
+
+            var timer = new Timer(OnTimerElapsed, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+            do
+            {
+                signaled = waitHandle.WaitOne(TimeSpan.FromMilliseconds(300));
+            } while (!signaled);            
+        }
+
+        private static void OnTimerElapsed(object state)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "netsh.exe";
+            p.StartInfo.Arguments = "wlan show interfaces";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+
+            if (!output.Contains("disconnected"))
+            {
+                ssid.Name = gv(output, "SSID");
+                string oldBSSID = ssid.BSSID;
+                string newBSSID = gv(output, "BSSID");
+                string signal = gv(output, "Signal");
+
+                if (!connected)
+                {
+                    connected = true;
+
+                    string message = "Wi-Fi connected to " + ssid.Name + " " + ssid.BSSID;
+                    Console.WriteLine(message);
+                    ShowToast(message);
+                }
+                
+                if (ssid.BSSID != newBSSID)
+                {
+                    ssid.BSSID = newBSSID;
+                    string message = "Roam on " + ssid.Name + " from " + oldBSSID + " to " + newBSSID;
+                    Console.WriteLine(message);
+                    ShowToast(message);
+                }
+                
+                p.WaitForExit();
+            }
+            else
+            {
+                if (connected)
+                {
+                    connected = false;
+                    Console.WriteLine("Wi-Fi is disconnected");
+                    ShowToast("Wi-Fi is disconnected");
+                    p.WaitForExit();
+                }
+
+                p.WaitForExit();
+            }
+        }
+
+        private static string gv(string output, string lookup)
+        { 
+            string s = output.Substring(output.IndexOf(lookup));
+            s = s.Substring(s.IndexOf(":"));
+            return s.Substring(2, s.IndexOf("\n")).Trim();
         }
 
         private static void ShowToast(string message)
@@ -51,13 +134,6 @@ namespace bssid
             var toast = new ToastNotification(toastXml);
 
             ToastNotificationManager.CreateToastNotifier("BSSID App").Show(toast);
-        }
-
-        private static string gv(string output, string lookup)
-        {
-            string s = output.Substring(output.IndexOf(lookup));
-            s = s.Substring(s.IndexOf(":"));
-            return s.Substring(2, s.IndexOf("\n")).Trim();
         }
     }
 }
---
+++ bssid/SSID.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace bssid
+{
+    public class SSID : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        private string bssid;
+
+        public SSID()
+        {
+
+        }
+
+        public SSID(string name, string bssid)
+        {
+            this.name = name;
+            this.bssid = bssid;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string BSSID
+        {
+            get { return bssid; }
+            set
+            {
+                bssid = value;
+                OnPropertyChanged("BSSIDValue");
+            }
+        }
+
+        protected void OnPropertyChanged(string bssid)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(bssid));
+            }
+        }
+    }
+}
